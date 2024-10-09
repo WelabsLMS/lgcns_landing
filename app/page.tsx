cbc80509373e15c7a478822ac9e8f3f00c6615cb
박노명 <pakc918@naver.com>
2024-10-09 19:49:14 +0900

    fix : build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import Footer from "@/components/layouts/Footer";
 import MainBanner from "@/components/page/Banner/MainBanner";
-import CampBenefit from "@/components/page/campbenefit/CampBenefit";
+// import CampBenefit from "@/components/page/campbenefit/CampBenefit";
 import Contact from "@/components/page/contact/Contact";
 import CoreProgram from "@/components/page/coreprogram/CoreProgram";
 import Faq from "@/components/page/faq/Faq";
 import CenterImageList from "@/components/page/imageofcenter/CenterImageList";
 import Curriculum from "@/components/page/introduce/Curriculum";
 import Introduce from "@/components/page/introduce/Introduce";
-import Location from "@/components/page/location/Location";
+// import Location from "@/components/page/location/Location";
 import MainApply from "@/components/page/mainApply/MainApply";
 import OverView from "@/components/page/overview/OverView";
 import Selection from "@/components/page/selection/Selection"; 
