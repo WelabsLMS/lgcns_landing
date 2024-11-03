4fef1fcbd64846ccbf4ccd6887b83d53fc308270
Kwak-Minju <ruri9898@gmail.com>
2024-11-03 09:43:27 +0900

    구분선 수정

--- components/ui/overview/OverViewEle.tsx
+++ components/ui/overview/OverViewEle.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function OverViewEle({ title, contents, id }: { title: string, contents: string, id: number }) {
     return (
-        <div className={`whitespace-pre text-wrap text-center flex flex-col items-center p-4 ${id == 1 || id==3 ? 'border-r':''} ${id == 1 || id == 2 ? 'border-b md:border-b-0':''} border-dotted border-[#00227830]`}>
+        <div className={`whitespace-pre text-wrap text-center flex flex-col items-center p-4 ${id == 1 && 'border-r border-b md:border-b-0'} ${id == 2 && 'md:border-r border-b md:border-b-0'} ${id == 3 && 'border-r'} border-dotted border-[#00227830]`}>
             <div className='w-[100%] flex flex-col items-center'>
                 <Image src={'/assets/images/overview/cloudIcon.svg'} alt={'cloud'} width={25} height={25} className='block md:hidden'/>
                 <Image src={'/assets/images/overview/cloudIcon.svg'} alt={'cloud'} width={30} height={30} className='hidden md:block'/>
