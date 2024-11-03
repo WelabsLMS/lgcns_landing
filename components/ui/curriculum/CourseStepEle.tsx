6e789541d69032ea5f55d17aad10f76b73ec6343
Kwak-Minju <ruri9898@gmail.com>
2024-11-03 11:53:25 +0900

    커리큘럼 모바일 수정

--- components/ui/curriculum/CourseStepEle.tsx
+++ components/ui/curriculum/CourseStepEle.tsx
@@ -25,19 +25,47 @@ function CourseStepEle({ title, subtitle, index, activeSlide, handleCourseClick 
     }, [activeSlide, index]);
 
     return (
-        <div className={`cursor-pointer relative lg:w-32 xl:w-full flex items-center lg:items-start text-nowrap`} onClick={() => handleCourseClick(index)}>
-            <div ref={stepRef} className='w-full h-min flex flex-col items-center gap-2 md:gap-3'>
-                <div className={`w-4 h-4 md:w-6 md:h-6 lg:w-8 lg:h-8 rounded-full ${index === activeSlide ? 'bg-[#002278]' : 'bg-[#E0E0E0]'}`} />
-                <div className={`lg:w-28 xl:w-36 rounded-lg lg:outline-dashed p-2 lg:p-4 ${index === activeSlide ? 'outline-[#002278]' : 'outline-[#E0E0E0]'}`}>
-                    <p className="text-[12px] md:text-base xl:text-2xl font-bold text-center mb-2 lg:mb-4">{title}</p>
-                    <p className='text-center text-[12px] lg:text-sm xl:text-xl font-semibold whitespace-pre-line'>{subtitle}</p>
+        <>
+            <div className={`cursor-pointer relative lg:w-32 xl:w-full hidden md:flex items-center lg:items-start text-nowrap`} onClick={() => handleCourseClick(index)}>
+                <div ref={stepRef} className='w-full h-min flex flex-col items-center gap-2 md:gap-3'>
+                    <div className={`w-4 h-4 md:w-6 md:h-6 lg:w-8 lg:h-8 rounded-full ${index === activeSlide ? 'bg-[#002278]' : 'bg-[#E0E0E0]'}`} />
+                    <div className={`lg:w-28 xl:w-36 rounded-lg lg:outline-dashed p-2 lg:p-4 ${index === activeSlide ? 'outline-[#002278]' : 'outline-[#E0E0E0]'}`}>
+                        <p className="text-[12px] md:text-base xl:text-2xl font-bold text-center mb-2 lg:mb-4">{title}</p>
+                        <p className='text-center text-[12px] lg:text-sm xl:text-xl font-semibold whitespace-pre-line'>{subtitle}</p>
+
+
+                    </div>
+                </div>
+                {
+                    index !== 6 &&
+                    <span className={`absolute -right-1 md:-right-1/3 md:translate-x-1/2 xl:-right-1 md:w-8 lg:right-0 w-4 lg:w-12 xl:w-16 h-0.5 top-[6.5px] md:top-[9px] lg:top-3 bg-[#E0E0E0] transform translate-x-1/2`}></span>
+                }
+            </div>
+
+            <div className="overflow-hidden md:hidden">
+                <div className={`cursor-pointer relative flex items-center text-nowrap`} onClick={() => handleCourseClick(index)}>
+                    <div ref={stepRef} className='w-full h-[180px] flex flex-col items-center gap-0'>
+                        <div className={`h-[40%] lg:w-28 xl:w-36 rounded-lg`}>
+                            <div className={`${index % 2 === 0 && 'hidden'}`}>
+                                <p className="text-[12px] font-bold text-center mb-2">{title}</p>
+                                <p className='text-center text-[12px] font-semibold whitespace-pre-line'>{subtitle}</p>
+                            </div>
+                        </div>
+                        <div className='h-[20%] flex items-center'>
+                            <div className={`w-3 h-3 rounded-full ${index === activeSlide ? 'bg-[#002278]' : 'bg-[#E0E0E0]'}`} />
+                            {
+                                index !== 6 &&
+                                <span className={`absolute -right-0.5 w-4 h-0.5 top-[90px] bg-[#E0E0E0] transform translate-x-1/2`}></span>
+                            }
+                        </div>
+                        <div className={`h-[40%] rounded-lg lg:outline-dashed ${index === activeSlide ? 'outline-[#002278]' : 'outline-[#E0E0E0]'} ${index % 2 !== 0 && 'hidden'}`}>
+                            <p className="text-[12px] font-bold text-center mb-2">{title}</p>
+                            <p className='text-center text-[12px] font-semibold whitespace-pre-line'>{subtitle}</p>
+                        </div>
+                    </div>
                 </div>
             </div>
-            {
-                index !== 6 &&
-                <span className={`absolute -right-1 md:-right-1/3 md:translate-x-1/2 xl:-right-1 md:w-8 lg:right-0 w-4 lg:w-12 xl:w-16 h-0.5 top-[6.5px] md:top-[9px] lg:top-3 bg-[#E0E0E0] transform translate-x-1/2`}></span>
-            }
-        </div>
+        </>
     );
 }
 
