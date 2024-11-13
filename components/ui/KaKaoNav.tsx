16fe758037d7ea6294f3f7dfd8c75fdf3d23386a
박노명 <pakc918@naver.com>
2024-11-13 14:57:41 +0900

    fix : header btn position

--- components/ui/KaKaoNav.tsx
+++ components/ui/KaKaoNav.tsx
@@ -6,7 +6,7 @@ export default function KaKaoNav() {
     return (
         <div
             onClick={() => { window.location.href = 'https://pf.kakao.com/_wbxkln' }}
-            className='fixed bottom-20 right-5 lg:bottom-32 lg:right-10 w-10 h-10 lg:w-16 lg:h-16 flex items-center justify-center z-[1000] cursor-pointer'>
+            className='fixed bottom-[4.5rem] right-5 lg:bottom-32 lg:right-10 w-10 h-10 lg:w-16 lg:h-16 flex items-center justify-center z-[1000] cursor-pointer'>
             {/* generate scroll to top icon */}
             <Image src='/assets/images/layout/kakaotalk.png' fill alt={'kakaotalk'} />
         </div>
