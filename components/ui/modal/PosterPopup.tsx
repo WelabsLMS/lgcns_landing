9d18a2c28443cd36271540de1efd7bf924b036a2
박노명 <pakc918@naver.com>
2024-11-18 20:30:00 +0900

    fix : poster 크기

--- components/ui/modal/PosterPopup.tsx
+++ components/ui/modal/PosterPopup.tsx
@@ -1,11 +1,19 @@
 'use client'
 // import { cookies } from 'next/headers'
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function PosterPopup() {
     const [isShow, setIsShow] = React.useState(true)
-    
+    const [isCookieChecked, setIsCookieChecked] = React.useState(false)
+
+    useEffect(() => {
+        if (document.cookie.includes('popup=close')) {
+            setIsShow(false)
+        }
+        setIsCookieChecked(true)
+    }, [])
+
     const closePopupToday = () => {
         // 한국 시간 기준으로 당일 11:59:59 까지 쿠키 유지
         const today = new Date()
@@ -22,13 +30,15 @@ export default function PosterPopup() {
         window.open('/assets/images/poster/LGCNS_AM_POSTER.pdf', '_blank')
     }
 
-    if (!isShow || document.cookie.includes('popup=close')) return null
+    if (!isShow || !isCookieChecked) return null
     return (
-        <div className='absolute md:left-1/4 lg: lg:left-1/3 border bg-white rounded-lg z-[990]'>
-            <Image src='/assets/images/poster/LGCNS_AM_POSTER.svg' alt='포스터 이미지' width={600} height={800} onClick={newWindow} className='cursor-pointer'/>
-            <div className='flex'>
-                <button className='w-full bg-gray-400 border text-white font-bold text-lg py-2' onClick={closePopupToday}>오늘 그만보기</button>
-                <button className='w-full bg-gray-400 border text-white font-bold text-lg py-2' onClick={()=>setIsShow(!isShow)} >닫기</button>
+        <div className='absolute inset-0 flex items-center justify-center z-[990]'>
+            <div className='absolute w-[360px] md:w-[500px] lg:w-[650px] top-[80px] lg:top-[100px] border bg-white rounded-lg z-[990]'>
+                <Image src='/assets/images/poster/LGCNS_AM_POSTER.svg' alt='포스터 이미지' width={650} height={800} onClick={newWindow} className='cursor-pointer' />
+                <div className='flex'>
+                    <button className='w-full bg-gray-400 border text-white font-bold text-lg py-2' onClick={closePopupToday}>오늘 그만보기</button>
+                    <button className='w-full bg-gray-400 border text-white font-bold text-lg py-2' onClick={() => setIsShow(!isShow)} >닫기</button>
+                </div>
             </div>
         </div>
     )
