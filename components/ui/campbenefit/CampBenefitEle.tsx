0607135b475d8c471362cd03bcff95bb9e616a27
박노명 <pakc918@naver.com>
2024-11-18 17:19:20 +0900

    fix : 훈련생 혜택 서류 삭제 및 해커톤 이미지 변경

--- components/ui/campbenefit/CampBenefitEle.tsx
+++ components/ui/campbenefit/CampBenefitEle.tsx
@@ -12,7 +12,7 @@ function CampBenefitEle({ benefitData }: { benefitData: CampBenefitEleProps }) {
     const imgName = benefitData.title.split('/').slice(-1)[0].split('.')[0]
 
     return (
-        <div className={`w-full md:w-[49%] lg:w-[350px] bg-[#C2D3FF50] rounded-3xl py-4`}>
+        <div className={`w-full md:w-[49%] lg:w-[350px] lg:max-w-1/3 bg-[#C2D3FF50] rounded-3xl py-4`}>
             <div className='flex flex-col items-center gap-5 font-semibold'>
                 <div className='relative bg-white rounded-full w-16 h-16 flex justify-center'>
                     <Image src={benefitData.imgSrc} alt={imgName} width={35} height={35} />
